54660a5d22c463610455b31fb2028d7ddc09c531
lubomirb <lubomirb@gmail.com>
2026-05-30 22:46:26 +0200

    slovnik z GigHub

--- ScrabbleSkX/Program.cs
+++ ScrabbleSkX/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScrabbleSkX;
-//using ScrabbleSkX.Services;
+using ScrabbleSkX.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped<FileServices>();
+builder.Services.AddScoped<DictionaryService>();
 
 await builder.Build().RunAsync();
---
+++ ScrabbleSkX/Services/DictionaryService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ScrabbleSkX.Services;
+
+public class DictionaryService
+{
+    private readonly HttpClient _http;
+    private string[]? _cachedWords;
+
+    public DictionaryService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<string[]> GetWordsAsync()
+    {
+        if (_cachedWords is not null)
+            return _cachedWords;
+
+        var url = "https://lubomirb.github.io/SlovnikSK/slovnik.txt";
+        var text = await _http.GetStringAsync(url);
+
+        _cachedWords = text
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+        return _cachedWords;
+    }
+}
